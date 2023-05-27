c38184cf507bd141ca1d77ad371adee6aa2b6b2c
hesanghasemi <hesanghasemi99@gmail.com>
2023-05-28 00:48:53 +0330

    add header section responsive

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import logo from "../assets/images/logo-white-orange.svg";
+import hamburgerMenu from "../assets/images/bars-solid.svg";
+import "../styles/Header.css";
+import Button from "./Button";
+
+const Header = () => {
+  return (
+    <header className="section header">
+      <img src={logo} alt="logo" className="headerLogo" />
+      <img src={hamburgerMenu} alt="hamburger menu" className="headerMenu" />
+      <nav className="headerNav">
+        <ul>
+          <li>Education</li>
+          <li>Experiences</li>
+          <li>Certifications</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <Button>Download CV</Button>
+    </header>
+  );
+};
+
+export default Header;
