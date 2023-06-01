c9e7dbcd0bebf5d9ea2e70995787ae654d443972
hesanghasemi <hesanghasemi99@gmail.com>
2023-06-01 13:46:29 +0330

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
